2b370a542985df3a5b35bfb8e4c2255c463f2e7a
Tobi van Bronswijk <t.vanbronswijk@student.fontys.nl>
2016-09-29 11:51:44 +0200

    DataBindings added.

--- app/src/main/java/questionablequality/rpglifeapp/MainMenuActivity.java
+++ app/src/main/java/questionablequality/rpglifeapp/MainMenuActivity.java
@@ -1,13 +1,33 @@
 package questionablequality.rpglifeapp;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.annotationprocessor.ProcessDataBinding;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import questionablequality.rpglifeapp.databinding.ActivityMainMenuBinding;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    ActivityMainMenuBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main_menu);
+
+        binding.MainMenuQuestLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vibrator vib = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+                vib.vibrate(1000);
+            }
+        });
+
+
     }
 }
