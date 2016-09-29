d6402cb143839c4bd4a7baa6998a1c91d88e0643
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2016-09-29 12:15:19 +0200

    Add text to main menu buttons

--- app/src/main/java/questionablequality/rpglifeapp/MainMenuActivity.java
+++ app/src/main/java/questionablequality/rpglifeapp/MainMenuActivity.java
@@ -1,10 +1,9 @@
 package questionablequality.rpglifeapp;
 
 import android.databinding.DataBindingUtil;
-import android.databinding.annotationprocessor.ProcessDataBinding;
+import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import questionablequality.rpglifeapp.databinding.ActivityMainMenuBinding;
@@ -20,7 +19,7 @@ public class MainMenuActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main_menu);
 
-        binding.MainMenuQuestLog.setOnClickListener(new View.OnClickListener() {
+        binding.btnQuestLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Vibrator vib = (Vibrator)getSystemService(VIBRATOR_SERVICE);
